99d6dba65051730f60a711aff35c429e930fa864
kilobyte-disk <kinsol.mail@Gmail.com>
2025-08-28 21:40:53 -0700

    Setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,66 @@
-#include <stdio.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class TestApp {
+public:
+	void run() {
+		InitWindow();
+		InitVulkan();
+		MainLoop();
+		Cleanup();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void InitWindow() {
+		glfwInit();
+		
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+		 /* NOTE: Wayland will NOT display the window until
+		  * a write to the compositor's buffer is attempted.
+		  */
+	}
+
+	void InitVulkan() {
+
+	}
+
+	void MainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void Cleanup() {
+		std::cout << "Cleaning up...\n";
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+};
 
 int main() {
-	printf("Hello vk-test!\n");
+	TestApp app;
 
-	return 0;
+	try {
+		app.run();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
